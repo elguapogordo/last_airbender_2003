21c7277970dc36608e8162b86ab4b4df3afbe801
Jesse Gietzen <gietzenjesse@gmail.com>
2020-07-14 09:24:32 -0600

    begin setup

---
+++ spec/features/search_for_members_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'as a user' do
+  it 'i can search for members' do
+    visit '/'
+    select 'Fire Nation', from: :nation
+    click_on 'commit'
+  end
+end
+
+# As a user,
+# When I visit "/"
+# And I Select "Fire Nation" from the select field
+# (Note: Use the existing select field)
+# And I click "Search For Members"
+# Then I should be on page "/search"
+# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
+# And I should see a list with the detailed information for the 20 members of the Fire Nation.
+# And for each of the members I should see:
+# - The name of the member (and their photo, if they have one)
+# - The list of allies and a list of enemies
+# - Any affiliation that the member has
